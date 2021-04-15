7266046b5f1af672e313c9e55b805c8722c56836
alambrow <alexander.lambrow@gmail.com>
2021-04-15 15:05:51 -0500

    first version of daily journal

---
+++ toy-objects/daily-notes/notes.js
@@ -0,0 +1,20 @@
+const Variable = [
+    {
+        id: 1,
+        subject: "Working with GitHub Repos",
+        date: "14 April 2021",
+        feeling: "a little fuzzy still, but getting there",
+        notes: "learned the basic workflow of GitHub; still unsure about how merging should work outside of the cloud",
+        timeSpent: "roughly three hours"
+    },
+    {
+        id: 2,
+        subject: "Data Structures",
+        date: "15 April 2021",
+        feeling: "fairly comfortable. have seen this before",
+        notes: "Introduction to Arrays const Array = [], including implementing arrays with key-vale pairs {} and how to embed arrays inside of one another [{},{},{}]",
+        timeSpent: "26 minutes"
+    }
+]
+
+console.log(Variable)
